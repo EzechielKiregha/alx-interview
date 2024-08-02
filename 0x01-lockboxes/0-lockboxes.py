182323ac85d9c558a8a0d9084958bad2110ad046
EzechielKiregha <ekiregha@gmail.com>
2024-08-02 02:39:42 +0200

    Why I need to be able to thing both inside and outside the box

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,8 @@
-#!/usr/bin/env python3
-"""
-In this module,
-You have n number of locked boxes in front of you.
-Each box is numbered sequentially from 0 to n - 1 and
-each box may contain keys to the other boxes.
-"""
+#!/usr/bin/python3
+"""this is an interview question on lockboxes"""
 
 
-from typing import List
-
-
-def canUnlockAll(boxes : List[List[int]]) -> bool:
+def canUnlockAll(boxes):
     """Ensure the use of a set for visited boxes
     for O(1) average-time complexity for checks.
     Use iterative DFS or BFS to avoid stack overflow issues
@@ -23,15 +15,45 @@ def canUnlockAll(boxes : List[List[int]]) -> bool:
     Returns:
         bool: True if all boxes can be opened, else return False
     """
-    visited = set()
-    stack = [0]
+    from copy import deepcopy
 
-    while stack:
-        box = stack.pop()
-        if box not in visited:
-            visited.add(box)
-            for key in boxes[box]:
-                if key < len(boxes) and key not in visited:
-                    stack.append(key)
-    
-    return len(visited) == len(boxes)
+    # checks if given valid list of boxes
+    if type(boxes) is not list or len(boxes) < 1:
+        return False
+    for box in boxes:
+        if type(box) is not list:
+            return False
+    # creates a copy of boxes to not affect the original list of lists
+    copy_boxes = deepcopy(boxes)
+    # creates list of available keys (only contains 0 initially)
+    keys_list = [0]
+    # while there are still available keys:
+    while len(keys_list) > 0:
+        # the current key will be the first available key
+        key = keys_list[0]
+        # keys list will reset to remove used key
+        keys_list = keys_list[1:]
+        if type(key) is int and key >= 0:
+            copy_boxes[key].append(-1)
+            for new_key in copy_boxes[key]:
+                if new_key == -1:
+                    # if new key is -1 flag to marks as opened, continue
+                    continue
+                if new_key >= len(copy_boxes):
+                    # if new key is out of range for the boxes, continue
+                    continue
+                if -1 in copy_boxes[new_key] or new_key in keys_list:
+                    # if box key is known, continue
+                    continue
+                # update the list of available key
+                assert isinstance(new_key, object)
+                keys_list.append(new_key)
+            continue
+        # checks if given valid key
+        return False
+    for box in copy_boxes:
+        if -1 not in box:
+            # if a box is missing the -1 flag, it was not unlocked
+            return False
+    # returns true if all boxes in the previous loop were indicated as unlocked
+    return True
